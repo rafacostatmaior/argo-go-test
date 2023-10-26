141b4c5f6a3a330485278219488ba8696ba04ec2
tmaior <tlmaior@gmail.com>
2023-10-26 12:06:31 -0300

    infra-setup

---
+++ app/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "test",
+		})
+	})
+	r.Run(":8080")
+}
